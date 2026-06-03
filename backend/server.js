f84682e5cf419a147d2e3b226f2b42d3ebc54f52
Tanishaoza <tanishaoza9@gmail.com>
2026-06-03 19:42:55 +0530

    Prepare backend for deployment

--- backend/server.js
+++ backend/server.js
@@ -314,6 +314,8 @@ app.get("/history/:email", async (req, res) => {
   }
 });
 
-app.listen(5000, () => {
-  console.log("🚀 Running on http://localhost:5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Running on port ${PORT}`);
 });
